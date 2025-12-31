932322d02120507a4788902cdd92404059cf85f9
jai890216 <jai890216@gmail.com>
2025-12-31 22:09:12 +0900

    feature/api : routes_chat.py, settings.py, requests.py, llm_client.py, llm 모델 연동 체팅 세션 시작 api 구현

--- backend/app/schemas/requests.py
+++ backend/app/schemas/requests.py
@@ -5,8 +5,10 @@ from pydantic import BaseModel
 class SessionStartRequest(BaseModel):
     """세션 시작 요청"""
     category: str
-
-
+    #LLM Provider 선택 기본값은 'google', model_name 기본값은 'gemini-2.5-flash', openai, 'gpt-4o' 등으로 설정 가능.
+    provider: str = 'google'
+    model_name: str ='gemini-2.5-flash'
+    
 class ChatRequest(BaseModel):
     """채팅 메시지 요청"""
     session_id: str
